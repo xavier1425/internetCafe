4cef58ca44c6b9722238ecdcfd28536bcdabdf11
Xavier1425 <jumiya009@gmail.com>
2018-07-03 17:26:37 +0800

    test conform

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
+// set up ======================================================================
+// get all the tools we need
 const express = require('express')
-const PORT = process.env.PORT || 5000
-const bodyParser = require('body-parser')
-const multer = require('multer')
-const upload = multer()
-
-const routes = require('./routes/router')
 const app = express()
+const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose')
+const passport = require('passport')
+const flash = require('connect-flash')
 
-app.set('view engine', 'pug')
-app.set('views', './views')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(upload.array())
-app.use(express.static('public'))
+const configDB = require('./config/database.js')
 
-routes(app)
+// configuration ===============================================================
+mongoose.connect(configDB.url) // connect to our database
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+require('./config/passport')(passport) // pass passport for configuration
+
+// set up express application
+app.use(morgan('dev')) // log every request to the console
+app.use(cookieParser()) // read cookies (needed for auth)
+app.use(bodyParser()) // get information from html forms
+
+app.set('view engine', 'pug') // set up pug for templating
+
+// required for passport
+app.use(session({ secret: 'areyouhappynow'})) // session secret
+app.use(passport.initialize())
+app.use(passport.session()) // persistent login sessions
+app.use(flash()) // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport) // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(PORT, () => console.log(`Listening on Port ${ PORT }`))
